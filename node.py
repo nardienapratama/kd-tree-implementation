e3b7f85becdc37bb3f79b06f6cb0c6111eabef7f
Nardiéna Althafia Pratama <34739714+nardienapratama@users.noreply.github.com>
2021-05-23 07:13:49 +1000

    Created init method; created getters

--- node.py
+++ node.py
@@ -1,3 +1,13 @@
 class Node():
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, point, axis):
+        self.value = point
+        self.left = None
+        self.right = None
+        self.axis = axis
+        self.rectangle = None
+
+    def getValue(self):
+        return self.value
+    
+    def getAxis(self):
+        return self.axis
