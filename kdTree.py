0dd59533fbbfd52e4480d2444b3fbe511e341c20
Nardiéna Althafia Pratama <34739714+nardienapratama@users.noreply.github.com>
2021-05-23 07:13:25 +1000

    Created init method; created skeleton for main methods

--- kdTree.py
+++ kdTree.py
@@ -1,5 +1,37 @@
-
+from node import Node
 
 class KDTree():
-    def __init__(self):
-        
+    def __init__(self, data=None):
+        self.dimensions = 2
+        self.root = None
+        if data:
+            for point in data:
+                self.insert(point)
+    
+
+    def insert(self, point):
+        # IF THERE IS NO ROOT NODE YET, SET CURRENT POINT AS ROOT NODE
+        if not self.root:
+            self.root = Node(point, 'X')
+        else:
+            
+            
+
+        return
+
+    def balance(self):
+        return
+    
+    def delete(self):
+        return
+    
+    def performRangeSearch(self, range):
+        return
+    
+    def performKNNSearch(self, radius):
+        return
+
+    def drawTree(self):
+        return
+
+    
