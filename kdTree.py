c73ab06bde5e13933a68d2ec50e68724fe9a6612
Nardiéna Althafia Pratama <34739714+nardienapratama@users.noreply.github.com>
2021-05-24 01:47:18 +1000

    Fixed insert method and made visualizer method from forked repo

--- kdTree.py
+++ kdTree.py
@@ -1,10 +1,12 @@
 # References Used for Guidance: 
 ## https://runestone.academy/runestone/books/published/pythonds/Trees/SearchTreeImplementation.html
 
-from node import Node
-from collections import deque
+from pptree.pptree import print_tree_vertically
+from myNode import MyNode
+from ppbtree import *
 
-class KDTree():
+
+class KDTree:
     def __init__(self):
         self.dimensions = [0, 1]   
         self.root = None
@@ -13,7 +15,7 @@ class KDTree():
     def insert(self, point):
         # IF THERE IS NO ROOT NODE YET, SET CURRENT POINT AS ROOT NODE
         if self.root is None:
-            self.root = Node(point, self.dimensions[0], 1)
+            self.root = MyNode(point, self.dimensions[0], 1)
             self.treeHeight = 1
         else:
             self.insertPoint(self.root, point, 0)
@@ -25,54 +27,59 @@ class KDTree():
         # IF AXIS IS DIMENSION 0 ('X')
         if currentNode.getAxis() == self.dimensions[0]:
             # IF CURRENT NODE HAS A CHILD SUBTREE
-            if currentNode.right or currentNode.left:
-                if point[self.dimensions[0]] >= currentNode.getXVal():
+            if point[self.dimensions[0]] >= currentNode.getXVal():
+                if currentNode.right:
                     self.insertPoint(currentNode.right, point, heightCount)
                 else:
+                    heightCount += 1
+                    currentNode.right = MyNode(point, self.dimensions[1], heightCount)
+            else:
+                if currentNode.left:
                     self.insertPoint(currentNode.left, point, heightCount)
-
-            # IF CURRENT NODE HAS NO CHILDREN
-            else: 
-                heightCount += 1
-                if point[self.dimensions[0]] >= currentNode.getXVal():
-                    currentNode.right = Node(point, self.dimensions[1], heightCount)
                 else:
-                    currentNode.left = Node(point, self.dimensions[1], heightCount)
+                    heightCount += 1
+                    currentNode.left = MyNode(point, self.dimensions[1], heightCount)
+                
 
         # IF AXIS IS DIMENSION 1 ('Y') 
         else:
-            if currentNode.right or currentNode.left:
-                if point[self.dimensions[1]] >= self.root.getYVal():
+            if point[self.dimensions[1]] >= currentNode.getYVal():
+                if currentNode.right:
                     self.insertPoint(currentNode.right, point, heightCount)
                 else:
-                    self.insertPoint(currentNode.left, point, heightCount)
+                    heightCount += 1
+                    currentNode.right = MyNode(point, self.dimensions[0], heightCount)
             
             else:
-                heightCount += 1
-                if point[self.dimensions[1]] >= currentNode.getXVal():
-                    currentNode.right = Node(point, self.dimensions[0], heightCount)
+                if currentNode.left:
+                    self.insertPoint(currentNode.left, point, heightCount)
                 else:
-                    currentNode.left = Node(point, self.dimensions[0], heightCount)
+                    currentNode.left = MyNode(point, self.dimensions[0], heightCount)
 
-    # def visualize(self):
-    #     queue = deque()
-    #     queue.append(self.root)
-    #     while queue:
-    #         current = queue.popleft()
+        if heightCount > self.treeHeight:
+            self.treeHeight = heightCount
 
-    #         if current.left:
-    #             queue.append(current.left)
-    #         if current.right:
-    #             queue.append(current.right)
-            
-            
+    def visualize(self):
+        # CODE USED FROM THIS GITHUB REPOSITORY: https://github.com/clemtoy/pptree
+        root = self._visualize(self.root, None)
+        print_tree(root, nameattr='value')
 
 
-    #             (2,3)          -------6
-    #            /     \         ---5/ --5\ ---4
-    #           /       \        ---4/ --7\ ---3    
-    #       (1,10)     (3,5)     ---- 16    ------- finalRow: numLevels * 8   
-    #     return
+    def _visualize(self, currentNode, parentNode):
+        treeNode = Node(currentNode.value)
+
+        if not currentNode.hasChild:
+            return treeNode
+        else:
+            if currentNode.left:
+                # The tree visualizer's left child goes up, so we make our 
+                # left nodes correspond to the visualizer's right child instead (it'll go down).
+                treeNode.right = self._visualize(currentNode.left, treeNode)
+            if currentNode.right:
+                # The tree visualizer's right child goes down, so we make our 
+                # right nodes correspond to the visualizer's left child instead (it'll go up).
+                treeNode.left = self._visualize(currentNode.right, treeNode)
+            return treeNode
 
     def balance(self):
         return
@@ -89,4 +96,3 @@ class KDTree():
     def drawTree(self):
         return
 
-    
