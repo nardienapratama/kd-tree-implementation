040c2d4f8387466398d40e0e0a036251f050a6f1
Nardiéna Althafia Pratama <34739714+nardienapratama@users.noreply.github.com>
2021-05-24 01:46:02 +1000

    Renamed node file to MyNode

---
+++ myNode.py
@@ -0,0 +1,39 @@
+class MyNode():
+    def __init__(self, point, axis, height):
+        self.value = point
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.axis = axis
+        self.rectangle = None
+        self.height = height
+
+    def __repr__(self) -> str:
+        return "<NODE> - {}".format(self.getValue())
+
+    def __str__(self) -> str:
+        return "<NODE> - {}".format(self.getValue())
+
+    def hasChild(self):
+        return self.right or self.left
+
+    def getHeight(self):
+        return self.height
+
+    def getRightChild(self):
+        return self.right
+
+    def getLeftChild(self):
+        return self.left
+
+    def getValue(self):
+        return self.value
+
+    def getXVal(self):
+        return self.value[0]
+
+    def getYVal(self):
+        return self.value[1]
+    
+    def getAxis(self):
+        return self.axis
